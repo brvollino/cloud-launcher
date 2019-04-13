5f91a17b098b2a66de7f0c9c9888bebc8b398b86
Bruno <brvollino@gmail.com>
2019-04-13 13:01:33 -0300

    Fixes resource name

--- infra-config.tf
+++ infra-config.tf
@@ -5,12 +5,12 @@ provider "aws" {
   access_key="access_key",
   secret_key="secret_key"
 }
-resource "aws_instance" "access-log-analysis-service " {
+resource "aws_instance" "access-log-analysis-service" {
   count = 1
   ami = "ami-0cb572d80a7417822"
   instance_type = "t2.micro"
   availability_zone= "sa-east-1"
   tags {
-    Name = "access-log-analysis-service "
+    Name = "access-log-analysis-service"
   }
 }
