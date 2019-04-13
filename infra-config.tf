45de3a34c7e0a4d4114936a2d59b6837bc531c5b
Bruno <brvollino@gmail.com>
2019-04-13 13:43:18 -0300

    Remove availability zone param

--- infra-config.tf
+++ infra-config.tf
@@ -10,7 +10,6 @@ resource "aws_instance" "access-log-analysis-service" {
   count = 1
   ami = "ami-0cb572d80a7417822"
   instance_type = "t2.micro"
-  availability_zone = "sa-east-1"
   tags {
     Name = "access-log-analysis-service"
   }
