ebbdb680647cc70b4e9786edee857c885516c7ac
Bruno <brvollino@gmail.com>
2019-04-13 21:32:40 -0300

    Find AMI by name

--- infra-config.tf
+++ infra-config.tf
@@ -6,9 +6,18 @@ provider "aws" {
   secret_key = "${var.secret_key}",
   region = "sa-east-1"
 }
+
+data "aws_ami" "access_log_analysis_service_ami" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["access-log-analysis-service-*"]
+  }
+  owners = ["self"]
+}
 resource "aws_instance" "access-log-analysis-service" {
   count = 1
-  ami = "ami-0cb572d80a7417822"
+  ami = "${data.aws_ami.access_log_analysis_service_ami.id}"
   instance_type = "t2.micro"
   key_name = "vollino_aws"
   associate_public_ip_address = true
