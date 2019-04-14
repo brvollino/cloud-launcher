8fd9d79bea7421325c87d2c4d276744cf27df8bb
Bruno <brvollino@gmail.com>
2019-04-13 22:50:20 -0300

    Opens SSH port

--- infra-config.tf
+++ infra-config.tf
@@ -6,6 +6,30 @@ provider "aws" {
   secret_key = "${var.secret_key}",
   region = "sa-east-1"
 }
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
+resource "aws_security_group" "ssh" {
+  name   = "ssh"
+  vpc_id = "${aws_default_vpc.default.id}"
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = -1
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 data "aws_ami" "access_log_analysis_service_ami" {
   most_recent = true
@@ -21,6 +45,7 @@ resource "aws_instance" "access-log-analysis-service" {
   instance_type = "t2.micro"
   key_name = "vollino_aws"
   associate_public_ip_address = true
+  security_groups = ["ssh"]
   tags {
     Name = "access-log-analysis-service"
   }
