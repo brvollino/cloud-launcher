2b23cde44c5eaa386f9d6f9d498e9d08ef686c54
Bruno <brvollino@gmail.com>
2019-04-13 21:27:29 -0300

    Adds SSH key and associates public ipv4 to the service instance

--- infra-config.tf
+++ infra-config.tf
@@ -10,6 +10,8 @@ resource "aws_instance" "access-log-analysis-service" {
   count = 1
   ami = "ami-0cb572d80a7417822"
   instance_type = "t2.micro"
+  key_name = "vollino_aws"
+  associate_public_ip_address = true
   tags {
     Name = "access-log-analysis-service"
   }
